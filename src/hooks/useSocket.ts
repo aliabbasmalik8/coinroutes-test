e6ceaa5fad99d0540d50ba4d0010f0f9cdacb7eb
Ali <dev@Devs-Book.local>
2024-08-02 03:43:20 +0500

    fixed socket issue

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -1,55 +1,67 @@
-import { useEffect, useState } from 'react'
-import { products } from '../utils/utils'
-import { IOrderBook } from '../utils/types'
+import { useEffect, useState } from 'react';
+import { products } from '../utils/utils';
+import { IOrderBook } from '../utils/types';
+
 export const useSocket = () => {
-    const [orderBookData, setOrderBookData] = useState<IOrderBook[]>([])
-    const [socket, setSocket] = useState<WebSocket | null>(null)
-    const [selectedProduct, setSelectedProduct] = useState(products[0])
+    const [orderBookData, setOrderBookData] = useState<IOrderBook[]>([]);
+    const [selectedProduct, setSelectedProduct] = useState(products[0]);
+
     useEffect(() => {
-        if (socket) {
-            socket.close()
-            setOrderBookData([])
-        }
+        const ws = new WebSocket('wss://advanced-trade-ws.coinbase.com');
         const subscriptionMessage = JSON.stringify({
             type: 'subscribe',
             product_ids: [selectedProduct],
             channel: 'level2',
             jwt: process.env.COINBASE_JWT,
-        })
-        const ws = new WebSocket('wss://advanced-trade-ws.coinbase.com')
-        ws.onopen = () => {
-            console.log('websocket connection open')
-            ws.send(subscriptionMessage)
-        }
-        ws.onmessage = (event: MessageEvent) => {
-            let newOrderBook: IOrderBook[] = []
-            const response = JSON.parse(event.data)
+        });
+
+        const handleOpen = () => {
+            console.log('WebSocket connection open');
+            ws.send(subscriptionMessage);
+        };
+
+        const handleMessage = (event: MessageEvent) => {
+            const response = JSON.parse(event.data);
             if (response.events) {
-                response.events.map((event: { updates: IOrderBook[] }) => {
-                    newOrderBook = newOrderBook.concat(event.updates)
-                    return event
-                })
+                                    console.log('==> response.events', response.events[0].product_id)
+
+                const newOrderBook = response.events
+                    .filter((event: { product_id: string; updates: IOrderBook[] }) => event.product_id === selectedProduct)
+                    .flatMap((event: { updates: IOrderBook[] }) => event.updates);
+
+                if (newOrderBook.length > 0) {
+                    setOrderBookData((prevOrderBook) => [...prevOrderBook, ...newOrderBook]);
+                }
             }
-            if (newOrderBook.length > 0) {
-                setOrderBookData((prevOrderBook) => [
-                    ...prevOrderBook,
-                    ...newOrderBook,
-                ])
-            }
-        }
-        ws.onclose = () => {
-            console.log('WebSocket connection closed')
-        }
-        ws.onerror = (error) => {
-            console.error(`WebSocket error: ${error}`)
-        }
-        setSocket(ws)
-    }, [selectedProduct])
+        };
+
+        const handleClose = (event: CloseEvent) => {
+            console.log('WebSocket connection closed', event);
+        };
+
+        const handleError = (error: Event) => {
+            console.error(`WebSocket error: ${error}`);
+        };
+
+        ws.addEventListener('open', handleOpen);
+        ws.addEventListener('message', handleMessage);
+        ws.addEventListener('close', handleClose);
+        ws.addEventListener('error', handleError);
+
+        // Cleanup function to close WebSocket connection
+        return () => {
+            console.log('Closing WebSocket connection');
+            ws.removeEventListener('open', handleOpen);
+            ws.removeEventListener('message', handleMessage);
+            ws.removeEventListener('close', handleClose);
+            ws.removeEventListener('error', handleError);
+            ws.close();
+        };
+    }, [selectedProduct]);
+
     return {
         orderBookData,
         selectedProduct,
         setSelectedProduct,
-        setSocket,
-        socket,
-    }
-}
+    };
+};
